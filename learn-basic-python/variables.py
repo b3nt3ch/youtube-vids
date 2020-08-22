408ccdbf332874b75383697513d8e543c5f8d77a
Walid Benchaa <kazannaki@gmail.com>
2020-08-22 15:48:56 +0100

    learn basic python part 2

---
+++ learn-basic-python/variables.py
@@ -0,0 +1,50 @@
+# Variablles and data types
+
+'''
+Hello I am walid
+and this is a multi line comment
+I hope you enjoy the tutorial
+'''
+
+"""
+same
+multi 
+line coment
+"""
+
+# A variable is a container for a value
+
+price = 5
+
+name = "walid"
+NAME = "hello"
+_zed = 12
+"""
+Variable rules:
+ - Variable names are case sensitive
+ - Must start with a letter or underscore
+ - Can have numbers but cannot start with one
+"""
+
+price = 5
+a = 5             # int
+b = 2.5           # float
+name = "walid"    # string
+is_right = False   # bool
+
+# print(name)
+# print(is_right)
+# print(type(a))
+
+# print(type(name))
+
+# arithmetic operations
+# addition:            3 + 2
+# subtraction:         3 - 2
+# multiplication:      3 * 2
+# division:            3 / 2
+# Floor division:      3 // 2
+# exponent:            3 ** 2
+# modulus:             3 % 2
+
+print(4 % 2)
