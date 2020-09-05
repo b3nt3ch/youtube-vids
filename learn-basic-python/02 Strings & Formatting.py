eb6c4631e36c236df0af977a66916649abfbb8c0
Walid Benchaa <kazannaki@gmail.com>
2020-09-05 18:22:00 +0100

    3rd lesson in learn basic python series

---
+++ learn-basic-python/02 Strings & Formatting.py
@@ -0,0 +1,42 @@
+# Strings in Python are surrounded by single or double quotation marks.
+# Creation of a string
+name = "walid"
+name2 = 'everyone'
+
+# Concatenation
+
+# print('hello ' + "walid")
+
+# String formating
+# print("Hello {} {} ".format(name, name2))
+
+
+# F string
+# print(f"Hello {name} {name2} ")
+# String Methods
+
+s = "hello Python walid"
+
+# Capitalize first letter
+# print(s.capitalize())
+
+# Everything upper case
+# print(s.upper())
+
+# Everything lower case
+# print(s.lower())
+
+# Get length
+# print(len(s))
+
+# Replace
+# print(s.replace("Python", "Everyone"))
+
+# Split into a list
+# print(s.split())
+
+# Starts with
+# print(s.startswith('h'))
+
+# Ends with
+print(s.endswith('d'))
